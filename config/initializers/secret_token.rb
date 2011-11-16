ad21f85d6f055e2cb2e2e5592d438e79beffddda
Hsu H <hmhlaing@hotmail.com>
2011-11-16 01:24:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreBoard::Application.config.secret_token = '65416f1584fdc088f47a56135c9ca17e29de9791021ad9693e26b4f8f37e9d0f63af0f72751b99b36e2f7bc31ca6ecbcb6fe119256deb638cd9496d3e368ccf7'
